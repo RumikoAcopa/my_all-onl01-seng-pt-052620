b9360fbbf5cc8710608dc9684c0f69ae4abcc982
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-07 02:16:40 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length
-    yield(collection[1])
+    block_return_values << yield(collection[1])
     i = i + 1
   end
 end
